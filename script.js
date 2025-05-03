e364f1f48ba1dd22f84255789ce2962d2bdf0fb3
azarrstore <azarrstore@gmail.com>
2025-05-04 00:26:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-document.getElementById("darkModeToggle").addEventListener("click", () => {
+const toggle = document.getElementById("darkModeToggle");
+toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
+  toggle.innerHTML = document.body.classList.contains("dark")
+    ? '<i class="fas fa-sun"></i>'
+    : '<i class="fas fa-moon"></i>';
 });
